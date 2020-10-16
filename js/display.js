ced16dc0ee9a7850344f4e152d0a48510da6c57c
Lucia <lubersol@gmail.com>
2020-10-16 17:37:46 +0200

    eliminacion personajes.html para insertarla dentro de index.html, creacion funcion mostrar y ocultar div personajes

---
+++ js/display.js
@@ -0,0 +1,13 @@
+function display(){
+    let clic = 1;
+    if(clic==1){
+        document.getElementById("container").style.display='block';
+        document.getElementById("capaGeneral").style.display='none';
+        clic = clic+1;
+    }else{
+        document.getElementById("container").style.display='none';
+        document.getElementById("capaGeneral").style.display='block';
+        clic = 1;
+    }
+
+}
